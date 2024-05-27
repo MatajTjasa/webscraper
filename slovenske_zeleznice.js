6a88b041535e60f2da242fa8d4e7319d7715bef7
tjasamataj <tjasa.mataj@student.um.si>
2024-05-27 23:09:50 +0200

    Finished base scraping Slovenske železnice.

--- slovenske_zeleznice.js
+++ slovenske_zeleznice.js
@@ -7,14 +7,47 @@ async function scrapeDestinations() {
 
     // Navigate to the train schedule page
     await page.goto('https://potniski.sz.si/');
-    await page.goto('https://potniski.sz.si/', { waitUntil: 'networkidle2' });    // Click to make the entry dropdown options visible
+    await page.goto('https://potniski.sz.si/', { waitUntil: 'networkidle2' });
     await page.waitForSelector('#entry-station-selectized', { visible: true });
-    await page.click('#entry-station-selectized');
-    //await page.waitForSelector('.selectize-dropdown-content', { visible: true });
 
+    // date
+    await page.waitForSelector('#departure-date', { visible: true });
+    await page.evaluate(() => {
+        document.querySelector('#departure-date').value = '';
+    });
+    await page.type('#departure-date', '30.5.2024')
+    await delay(1000);
+
+
+    // Click on the input field to display the dropdown
+    await page.click('#entry-station-selectized');
     await page.waitForSelector('.selectize-dropdown-content', { visible: true });
 
-    // Extract entry station options
+    // Select the option with the text "Maribor"
+    await page.evaluate(() => {
+        const departures = Array.from(document.querySelectorAll('.selectize-dropdown-content .option'));
+        const departure = departures.find(option => option.textContent.trim() === 'Maribor');
+        if (departure) {
+            departure.click();
+        }
+    });
+
+    await delay(2000);
+
+    await page.waitForSelector('#exit-station-selectized', { visible: true });
+
+    // Click on the input field to display the dropdown
+    await page.click('#exit-station-selectized');
+
+    await page.evaluate(() => {
+        const arrivals = Array.from(document.querySelectorAll('.v-izstop .selectize-dropdown-content .option'));
+        const arrival = arrivals.find(option => option.textContent.trim() === 'Ljubljana');
+        if (arrival) {
+            arrival.click();
+        }
+    });
+
+    // Get the list of destinations after selection
     const destinations = await page.evaluate(() => {
         return Array.from(document.querySelectorAll('.selectize-dropdown-content .option')).map(option => ({
             value: option.getAttribute('data-value'),
@@ -22,7 +55,6 @@ async function scrapeDestinations() {
         }));
     });
 
-    // Log extracted destinations
     console.log(destinations);
 
     // Save destinations to JSON file
@@ -31,7 +63,42 @@ async function scrapeDestinations() {
         else console.log('Successfully written to slovenske_zeleznice_destinations.json');
     });
 
-    await browser.close();
-}
+    await page.click('button.btn.btn-primary[type="submit"]');
 
+    await page.waitForSelector('.connection', { visible: true });
+
+    // Scrape the train schedule data
+    const trainSchedules = await page.evaluate(() => {
+        const connections = Array.from(document.querySelectorAll('.connection'));
+        return connections.map(connection => {
+            const departureStation = connection.querySelector('.item.has-issues div:first-child strong').innerText;
+            const departureTime = connection.querySelector('.item.has-issues div:first-child strong:nth-child(2)').innerText;
+            const arrivalStation = connection.querySelector('.item:last-child strong').innerText;
+            const arrivalTime = connection.querySelector('.item:last-child strong:nth-child(2)').innerText;
+            const travelTime = connection.querySelector('.item.between div:first-child strong').innerText;
+            const trainType = connection.querySelector('.train-list-item .train-trigger').innerText;
+            return {
+                departureStation,
+                departureTime,
+                arrivalStation,
+                arrivalTime,
+                travelTime,
+                trainType
+            };
+        });
+    });
+
+    console.log(trainSchedules);
+
+    // Save the scraped data to a JSON file
+    fs.writeFile('train_schedules.json', JSON.stringify(trainSchedules, null, 2), err => {
+        if (err) console.log('Error writing file:', err);
+        else console.log('Successfully written to train_schedules.json');
+    });
+    //await browser.close();
+
+}
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 scrapeDestinations().catch(err => console.error('Error executing scrapeDestinations:', err));
