1370a9e3fc2d94c22d10c92d0d4acac8852a5307
tjasamataj <tjasa.mataj@student.um.si>
2024-08-15 20:35:54 +0200

    Frontend: Added some tailwind fixes. Added unversioned files. Added some babel fix.

--- frontend/src/components/Results.js
+++ frontend/src/components/Results.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Results({results}) {
     console.log('results data:', results);
 
-    if (!results) {
+    if (!results || !Array.isArray(results)) {
         return <div>No results found.</div>;
     }
 
@@ -19,14 +19,15 @@ function Results({results}) {
                 <table className="table-auto w-full bg-white rounded-md shadow-md">
                     <thead>
                     <tr>
-                        <th className="px-4 py-2 bg-blue-600 text-white">Time</th>
-                        <th className="px-4 py-2 bg-blue-600 text-white">Description</th>
-                        <th className="px-4 py-2 bg-blue-600 text-white">Price</th>
+                        <th className="px-4 py-2 bg-[#4682B4] text-white">Ura</th>
+                        <th className="px-4 py-2 bg-[#4682B4] text-white">Oseba</th>
+                        <th className="px-4 py-2 bg-[#4682B4] text-white">Cena</th>
                     </tr>
                     </thead>
                     <tbody>
                     {route.trips.map((trip, tripIndex) => (
-                        <tr key={tripIndex} className="hover:bg-gray-100">
+                        <tr key={tripIndex}
+                            className={`hover:bg-gray-100 ${tripIndex % 2 === 0 ? 'bg-white' : 'bg-gray-100'}`}>
                             <td className="border px-4 py-2">{trip.time}</td>
                             <td className="border px-4 py-2">{trip.description}</td>
                             <td className="border px-4 py-2">{trip.price}</td>
@@ -48,17 +49,18 @@ function Results({results}) {
                 <table className="table-auto w-full bg-white rounded-md shadow-md">
                     <thead>
                     <tr>
-                        <th className="px-4 py-2 bg-blue-600 text-white">Departure Station</th>
-                        <th className="px-4 py-2 bg-blue-600 text-white">Departure Time</th>
-                        <th className="px-4 py-2 bg-blue-600 text-white">Arrival Station</th>
-                        <th className="px-4 py-2 bg-blue-600 text-white">Arrival Time</th>
-                        <th className="px-4 py-2 bg-blue-600 text-white">Travel Time</th>
-                        <th className="px-4 py-2 bg-blue-600 text-white">Train Type</th>
+                        <th className="px-4 py-2 bg-[#4682B4] text-white">Departure Station</th>
+                        <th className="px-4 py-2 bg-[#4682B4] text-white">Departure Time</th>
+                        <th className="px-4 py-2 bg-[#4682B4] text-white">Arrival Station</th>
+                        <th className="px-4 py-2 bg-[#4682B4] text-white">Arrival Time</th>
+                        <th className="px-4 py-2 bg-[#4682B4] text-white">Travel Time</th>
+                        <th className="px-4 py-2 bg-[#4682B4] text-white">Train Type</th>
                     </tr>
                     </thead>
                     <tbody>
                     {trains.map((train, index) => (
-                        <tr key={index} className="hover:bg-gray-100">
+                        <tr key={index}
+                            className={`hover:bg-gray-100 ${train.isActive ? '' : 'bg-gray-200'} ${index % 2 === 0 ? 'bg-white' : 'bg-gray-100'}`}>
                             <td className="border px-4 py-2">{train.departureStation}</td>
                             <td className="border px-4 py-2">{train.departureTime}</td>
                             <td className="border px-4 py-2">{train.arrivalStation}</td>
@@ -79,21 +81,21 @@ function Results({results}) {
         }
         return (
             <div className="result-section mb-8">
-                <h2 className="text-2xl font-semibold mb-4">APMS Results</h2>
+                <h2 className="text-2xl font-semibold mb-4">Adsadsfs</h2>
                 <table className="table-auto w-full bg-white rounded-md shadow-md">
                     <thead>
                     <tr>
-                        <th className="px-4 py-2 bg-blue-600 text-white">Departure</th>
-                        <th className="px-4 py-2 bg-blue-600 text-white">Departure Time</th>
-                        <th className="px-4 py-2 bg-blue-600 text-white">Arrival</th>
-                        <th className="px-4 py-2 bg-blue-600 text-white">Arrival Time</th>
-                        <th className="px-4 py-2 bg-blue-600 text-white">Duration</th>
-                        <th className="px-4 py-2 bg-blue-600 text-white">Kilometers</th>
+                        <th className="px-4 py-2 bg-[#4682B4] text-white">Departure</th>
+                        <th className="px-4 py-2 bg-[#4682B4] text-white">Departure Time</th>
+                        <th className="px-4 py-2 bg-[#4682B4] text-white">Arrival</th>
+                        <th className="px-4 py-2 bg-[#4682B4] text-white">Arrival Time</th>
+                        <th className="px-4 py-2 bg-[#4682B4] text-white">Duration</th>
+                        <th className="px-4 py-2 bg-[#4682B4] text-white">Kilometers</th>
                     </tr>
                     </thead>
                     <tbody>
                     {apms.map((bus, index) => (
-                        <tr key={index} className="hover:bg-gray-100">
+                        <tr key={index} className={`hover:bg-gray-100 ${index % 2 === 0 ? 'bg-white' : 'bg-gray-100'}`}>
                             <td className="border px-4 py-2">{bus.departure}</td>
                             <td className="border px-4 py-2">{bus.departureTime}</td>
                             <td className="border px-4 py-2">{bus.arrival}</td>
@@ -118,19 +120,19 @@ function Results({results}) {
                 <table className="table-auto w-full bg-white rounded-md shadow-md">
                     <thead>
                     <tr>
-                        <th className="px-4 py-2 bg-blue-600 text-white">Departure</th>
-                        <th className="px-4 py-2 bg-blue-600 text-white">Departure Time</th>
-                        <th className="px-4 py-2 bg-blue-600 text-white">Arrival</th>
-                        <th className="px-4 py-2 bg-blue-600 text-white">Arrival Time</th>
-                        <th className="px-4 py-2 bg-blue-600 text-white">Travel Duration</th>
-                        <th className="px-4 py-2 bg-blue-600 text-white">Prevoznik</th>
-                        <th className="px-4 py-2 bg-blue-600 text-white">Length</th>
-                        <th className="px-4 py-2 bg-blue-600 text-white">Price</th>
+                        <th className="px-4 py-2 bg-[#4682B4] text-white">Departure</th>
+                        <th className="px-4 py-2 bg-[#4682B4] text-white">Departure Time</th>
+                        <th className="px-4 py-2 bg-[#4682B4] text-white">Arrival</th>
+                        <th className="px-4 py-2 bg-[#4682B4] text-white">Arrival Time</th>
+                        <th className="px-4 py-2 bg-[#4682B4] text-white">Travel Duration</th>
+                        <th className="px-4 py-2 bg-[#4682B4] text-white">Prevoznik</th>
+                        <th className="px-4 py-2 bg-[#4682B4] text-white">Length</th>
+                        <th className="px-4 py-2 bg-[#4682B4] text-white">Cena</th>
                     </tr>
                     </thead>
                     <tbody>
                     {arriva.map((bus, index) => (
-                        <tr key={index} className="hover:bg-gray-100">
+                        <tr key={index} className={`hover:bg-gray-100 ${index % 2 === 0 ? 'bg-white' : 'bg-gray-100'}`}>
                             <td className="border px-4 py-2">{bus.departure}</td>
                             <td className="border px-4 py-2">{bus.departureTime}</td>
                             <td className="border px-4 py-2">{bus.arrival}</td>
