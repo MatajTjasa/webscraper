afad16fad6422fb3f4cc8a227f5e46657843e78a
tjasamataj <tjasa.mataj@student.um.si>
2024-09-10 20:55:16 +0200

    Fixed dark mode on mobile view for background.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -37,15 +37,61 @@ function App() {
         }}></div>
     ));
 
+    useEffect(() => {
+        const setVh = () => {
+            let vh = window.innerHeight * 0.01;
+            document.documentElement.style.setProperty('--vh', `${vh}px`);
+        };
+
+        setVh();
+        window.addEventListener('resize', setVh);
+
+        return () => window.removeEventListener('resize', setVh);
+    }, []);
+
     return (
         <DestinationsProvider>
             <Router>
+                {darkMode ? (
+                    <div style={{
+                        width: '100vw',
+                        height: 'calc(var(--vh, 1vh) * 100)',
+                        backgroundColor: '#000000FF',
+                        backgroundImage: 'linear-gradient(to bottom right, #071526 10%, #030d18 30%, #29056d 50%, #000000 80%, #000 100%)',
+                        transition: 'background 0.5s ease, color 0.5s ease',
+                        backgroundAttachment: 'fixed',
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                        backgroundRepeat: 'no-repeat',
+                        color: '#ccc',
+                        zIndex: '-1',
+                        position: 'fixed',
+                        top: '0'
+                    }}></div>
+                ) : (
+                    <div style={{
+                        width: '100vw',
+                        height: 'calc(var(--vh, 1vh) * 100)',
+                        backgroundColor: '#1E8EFD',
+                        overflowX: 'hidden',
+                        overflowY: 'auto',
+                        backgroundImage: 'linear-gradient(to bottom right, #fff, #eae7df 15%, #6DB3F2 60%, #1E8EFD 80%, #B4ECF0)',
+                        transition: 'background 0.5s ease, color 0.5s ease',
+                        backgroundAttachment: 'fixed',
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                        backgroundRepeat: 'no-repeat',
+                        zIndex: '-1',
+                        position: 'fixed',
+                        top: '0'
+                    }}></div>
+                )}
                 <div className="flex justify-between items-center p-4">
                     <button
                         className="text-2xl text-gray-800 dark:text-white hover:text-blue-500 absolute top-4 right-4"
                         onClick={toggleDarkMode}
                     >
-                        {darkMode ? '🌙' : '☀️'}
+                        {!darkMode ? '🌙' : '☀️'}
                     </button>
                 </div>
                 {darkMode && <div className="stars">{stars}</div>}
@@ -55,7 +101,7 @@ function App() {
                 </Routes>
                 <footer className="italic mt-8 py-4 text-center text-gray-100 text-base">
                     <p>
-                        Vsi urniki javnega prevoza so javno dostopni. Ta stran jih le prikazuje na enem mestu.
+                        Vsi urniki javnega prevoza so javno dostopni. Ta stran jih le prikazuje na enem mestu.1
                     </p>
                 </footer>
             </Router>
