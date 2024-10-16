69aa2e0c04465b5da73602cc18ca91bc1d44b0a1
tjasamataj <tjasa.mataj@student.um.si>
2024-10-16 18:57:05 +0200

    Removed puppeteer usage from Prevozi and Arriva.

--- backend/scrapers/arriva_byUrl.js
+++ backend/scrapers/arriva_byUrl.js
@@ -1,56 +1,10 @@
 const axios = require('axios');
-const cheerio = require('cheerio');
-const puppeteer = require('puppeteer-extra');
-const StealthPlugin = require('puppeteer-extra-plugin-stealth');
-const RecaptchaPlugin = require('puppeteer-extra-plugin-recaptcha');
-const {safeGoto, delay, formatLocation} = require('../server/helpers');
+const {formatLocation} = require('../server/helpers');
 const {getCodeArriva} = require('../server/database.js');
-require('dotenv').config();
-
-// Hiding puppeteer usage
-puppeteer.use(StealthPlugin());
-
-puppeteer.use(
-    RecaptchaPlugin({
-        provider: {
-            id: '2captcha',
-            token: process.env.CAPTCHA_APIKEY
-        },
-        visualFeedback: true
-    })
-);
+const cheerio = require('cheerio');
 
 async function scrapeArrivaByUrl(departure, destination, date) {
-    let browser;
     try {
-        browser = await puppeteer.launch({
-            headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox', '--incognito'],
-            executablePath: puppeteer.executablePath()
-        });
-
-        const context = browser.defaultBrowserContext();
-        const page = (await context.pages())[0]
-
-        console.log("Navigating to the Arriva website...");
-        await safeGoto(page, 'https://arriva.si/vozni-redi/');
-        await delay(3000);
-
-        // Handle accept button
-        /*        try {
-                    await page.waitForSelector("#CybotCookiebotDialogBodyLevelButtonAccept", {timeout: 10000});
-                    console.log("Accepting cookies...");
-
-                    const acceptButton = await page.$("#CybotCookiebotDialogBodyLevelButtonAccept");
-                    if (acceptButton) {
-                        await acceptButton.click();
-                    } else {
-                        console.log("Accept button not found or already removed.");
-                    }
-                } catch (error) {
-                    console.log("Accept button not found within the timeout period. Proceeding with the script...");
-                }*/
-
         const formattedDeparture = formatLocation(departure);
         const formattedDestination = formatLocation(destination);
 
@@ -59,92 +13,75 @@ async function scrapeArrivaByUrl(departure, destination, date) {
 
         if (!departureId || !destinationId) {
             console.log("One or both station codes could not be found in the database.");
-            await browser.close();
             return [];
         }
 
         console.log(`Extracted IDs - Departure ID: ${departureId}, Destination ID: ${destinationId}`);
 
-        const url = `https://arriva.si/vozni-redi/?departure-123=${formattedDeparture}&departure_id=${departureId}&departure=${formattedDeparture}&destination=${formattedDestination}&destination_id=${destinationId}&trip_date=${date}`;
-        console.log("Navigating to the URL:", url);
-        await safeGoto(page, url);
-        console.log("Current page URL:", page.url());
-
-        const noResults = await page.evaluate(() => {
-            const alertElement = document.querySelector('.alert.alert-danger');
-            return alertElement ? alertElement.textContent.trim() : null;
-        });
-
-        if (noResults) {
-            console.log("No direct connections found.");
-            await browser.close();
-            return [];
-        }
-
-        const connectionData = await fetchConnection(page.url());
-        await browser.close();
-        return connectionData;
-    } catch (error) {
-        if (browser) {
-            await browser.close();
-        }
-        console.error('Error in scrapeArrivaByUrl:', error);
-        throw error;
-    }
-}
-
-const fetchConnection = async (url) => {
-    try {
-        if (!url) {
-            console.log("URL is missing.");
-            return [];
-        }
+        // API URL
+        const url = `https://arriva.si/vozni-redi/?departure-123=${formattedDeparture}&departure_id=${departureId}&departure=${formattedDeparture}&destination=${formattedDestination}&destination_id=${destinationId}&trip_date=${encodeURIComponent(date)}`;
+        console.log("Arriva API request:", url);
 
         const response = await axios.get(url);
+
         const html = response.data;
-        const $ = cheerio.load(html);
-        const connectionData = [];
 
-        $('div.connection:not(.connection-header) .connection-inner').each((index, el) => {
-            const connection = $(el);
-
-            const departureTimeElement = connection.find('.departure-arrival .departure td span');
-            const departureTime = departureTimeElement.eq(0).text().trim();
-            const departure = departureTimeElement.parent().next().find('span').text().trim();
-
-            const arrivalTimeElement = connection.find('.departure-arrival .arrival td span');
-            const arrivalTime = arrivalTimeElement.eq(0).text().trim();
-            const arrival = arrivalTimeElement.parent().next().find('span').text().trim();
-
-            const travelDuration = connection.find('.duration .travel-duration span').text().trim();
-            const prevoznik = connection.find('.duration .prevoznik span').eq(1).text().trim();
-
-            const length = connection.find('.length').text().trim();
-            const price = connection.find('.price').text().trim();
-
-            connectionData.push({
-                departure,
-                departureTime,
-                arrival,
-                arrivalTime,
-                travelDuration,
-                prevoznik,
-                length,
-                price
-            });
-        });
-
-        // Check if connectionData is empty
-        if (connectionData.length === 0) {
-            console.log("No connections found.");
+        if (!html || html.includes("No direct connections")) {
+            console.log("No direct connections found or invalid data.");
             return [];
-        } else {
-            console.log(connectionData);
-            return connectionData;
         }
-    } catch (err) {
-        console.error(err);
+
+        // Extract bus schedule details from the response
+        const connectionData = await fetchConnectionData(html);
+        return connectionData;
+
+    } catch (error) {
+        console.error('Error in scrapeArrivaByUrl:', error);
         return [];
     }
 }
+
+// Helper function to parse the connection data from the HTML response
+const fetchConnectionData = (html) => {
+    const $ = cheerio.load(html);
+    const connectionData = [];
+
+    $('div.connection:not(.connection-header) .connection-inner').each((index, el) => {
+        const connection = $(el);
+
+        const departureTimeElement = connection.find('.departure-arrival .departure td span');
+        const departureTime = departureTimeElement.eq(0).text().trim();
+        const departure = departureTimeElement.parent().next().find('span').text().trim();
+
+        const arrivalTimeElement = connection.find('.departure-arrival .arrival td span');
+        const arrivalTime = arrivalTimeElement.eq(0).text().trim();
+        const arrival = arrivalTimeElement.parent().next().find('span').text().trim();
+
+        const travelDuration = connection.find('.duration .travel-duration span').text().trim();
+        const prevoznik = connection.find('.duration .prevoznik span').eq(1).text().trim();
+
+        const length = connection.find('.length').text().trim();
+        const price = connection.find('.price').text().trim();
+
+        connectionData.push({
+            departure,
+            departureTime,
+            arrival,
+            arrivalTime,
+            travelDuration,
+            prevoznik,
+            length,
+            price
+        });
+    });
+
+    if (connectionData.length === 0) {
+        console.log("No connections found.");
+        return [];
+    } else {
+        console.log(connectionData);
+        return connectionData;
+    }
+}
+
 module.exports = {scrapeArrivaByUrl};
--- backend/scrapers/prevozi_byUrl.js
+++ backend/scrapers/prevozi_byUrl.js
@@ -1,105 +1,39 @@
-const puppeteer = require('puppeteer-extra');
-const StealthPlugin = require('puppeteer-extra-plugin-stealth');
-const RecaptchaPlugin = require('puppeteer-extra-plugin-recaptcha');
-const {safeGoto} = require('../server/helpers');
-require('dotenv').config();
+const axios = require('axios');
 
-// Hiding puppeteer usage
-puppeteer.use(StealthPlugin());
-
-puppeteer.use(
-    RecaptchaPlugin({
-        provider: {
-            id: '2captcha',
-            token: process.env.CAPTCHA_APIKEY
-        },
-        visualFeedback: true
-    })
-);
-
-async function scrapePrevoziByUrl(departure, destination, date) {
-    const browser = await puppeteer.launch({
-        headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        executablePath: puppeteer.executablePath()
-    });
-    const page = await browser.newPage();
-
-    const url = `https://prevoz.org/prevoz/list/?fc=SI&f=${encodeURIComponent(departure)}&tc=SI&t=${encodeURIComponent(destination)}&d=${date}`;
-    console.log('Prevozi URL: ' + url);
-
+// TODO: add a solution with cheerio here and move fetchPrevozi elsewhere?
+async function fetchPrevozi(departure, destination, date) {
     try {
-        await safeGoto(page, url);
+        // API URL
+        const url = `https://prevoz.org/api/rides?from=${encodeURIComponent(departure)}&to=${encodeURIComponent(destination)}&date=${encodeURIComponent(date)}`;
+        console.log("Fetching data from URL:", url);
 
-        const error404 = await page.$eval('h1.fw-bolden.mb-4', element => element.innerText.includes('Napaka 404'));
-        if (error404) {
-            console.error('Error 404: Page not found');
-            await browser.close();
-            return [];
-        }
-        const cardExists = await page.$('.card');
-        if (!cardExists) {
-            console.log('No cards found');
-            await browser.close();
+        const response = await axios.get(url);
+
+        if (!response.data || response.data.length === 0) {
+            console.log("No ride shares found.");
             return [];
         }
 
-        const rideShares = await page.evaluate(() => {
-            const data = [];
-            const cards = document.querySelectorAll('.card');
-
-            cards.forEach(card => {
-                const cardBody = card.querySelector('.card-body');
-                if (cardBody && cardBody.innerText.includes('Noben prevoz ne ustreza iskalnim pogojem')) {
-                    data.push(null);
-                    return [];
-                }
-
-                const routeElement = card.querySelector('.d-flex.fw-bolden.h4.m-0');
-                if (!routeElement) {
-                    return [];
-                }
-                const fromElement = routeElement.querySelector('span:first-child');
-                const toElement = routeElement.querySelector('span:last-child');
-                if (!fromElement || !toElement) {
-                    return [];
-                }
-                const from = fromElement.innerText.trim();
-                const to = toElement.innerText.trim();
-                const countElement = card.querySelector('.card-header .count');
-                const count = countElement ? countElement.innerText.trim() : '0';
-                const trips = [];
-
-                const tripItems = card.querySelectorAll('.list-group-item.carshare-overview');
-                tripItems.forEach(item => {
-                    const timeElement = item.querySelector('.link-body');
-                    const descriptionElement = item.querySelector('.description.small');
-                    const priceElement = item.querySelector('.options .item span.h5.fw-bold.m-0');
-
-                    const time = timeElement ? timeElement.innerText.trim() : '';
-                    const description = descriptionElement ? descriptionElement.innerText.trim() : '';
-                    const price = priceElement ? priceElement.innerText.trim() : '';
-
-                    trips.push({time, description, price});
-                });
-
-                data.push({from, to, count, trips});
-            });
-
-            return data.filter(item => item !== null);
-        });
-        await browser.close();
+        // Formatting
+        const rideShares = response.data.map((ride, index) => ({
+            id: index + 1,
+            from: ride.from,
+            to: ride.to,
+            time: ride.time,
+            description: ride.description,
+            price: ride.price,
+        }));
 
         console.log(rideShares);
         return rideShares;
+
     } catch (error) {
-        console.error('Error executing scrapePrevozi:', error);
-        await browser.close();
+        console.error('Error fetching ride shares:', error);
         return [];
     }
 }
 
 // Example usage
-//scrapePrevozi_byUrl('Ljubljana', 'Maribor', '2024-08-04').catch(err => console.error('Error executing scrapePrevozi:', err));
+// fetchPrevozi('Ljubljana', 'Maribor', '2024-08-04').catch(err => console.error('Error:', err));
 
-module.exports = {scrapePrevoziByUrl};
+module.exports = {fetchPrevozi};
